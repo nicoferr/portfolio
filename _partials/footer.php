f2bb9285b5da04f77ac97d65eda64e31c526889e
Nicolas Ferrari <nico.ferr@hotmail.fr>
2025-10-04 21:16:35 +0200

    fixed footer

--- _partials/footer.php
+++ _partials/footer.php
@@ -1,5 +1,5 @@
         <footer id="footer">
-            © 2025 — Votre Nom. Portfolio HTML + CSS.
+            © <?= date('Y') ?> — Nicolas Ferrari. Portfolio HTML + CSS + PHP.
         </footer>
     </body>
 </html>
